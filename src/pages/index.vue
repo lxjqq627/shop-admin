2649cc14e760c11b9c0447534aca4501b46f3f52
xinjinliu <lxjqq627@126.com>
2022-08-15 23:35:53 +0800

    init: from lesson 16

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        后台首页 
+        <el-button @click="set">设置</el-button>
+        <el-button @click="get">读取</el-button>
+        <el-button @click="remove">删除</el-button>
+    </div>
+</template>
+<script setup>
+    import { useCookies } from '@vueuse/integrations/useCookies'
+    const cookie = useCookies()
+
+    console.log(cookie);
+
+    function set(){
+        cookie.set("admin-token","1232456")
+    }
+
+    function get(){
+        console.log(cookie.get("admin-token"));
+    }
+
+    function remove(){
+        cookie.remove("admin-token")
+    }
+</script>
