955455f4a7c1bb11f772e76b8ae38fbcd9669439
刘新金 <liuxj@cgt.com.cn>
2022-08-26 15:14:39 +0800

    to 31

--- src/layouts/components/FMenu.vue
+++ src/layouts/components/FMenu.vue
@@ -1,40 +1,84 @@
 <template>
-  <div>
+  <div class="f-menu">
     <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <el-sub-menu index="1">
+      default-active="2"
+      class="border-0"
+      @open="handleOpen"
+      @close="handleClose"
+      @select="handleSelect"
+    >
+      <template v-for="(item, index) in asideMenus" :key="index">
+        <el-sub-menu
+          v-if="item.child && item.child.length > 0"
+          :index="item.name"
+        >
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+            <el-icon>
+              <component :is="item.icon"></component>
+            </el-icon>
+            <span>{{ item.name }}</span>
           </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
+          <el-menu-item
+            v-for="(item2, index2) in item.child"
+            :key="index2"
+            :index="item2.frontpath"
+            >
+            <el-icon>
+              <component :is="item2.icon"></component>
+            </el-icon>
+            <span>{{ item2.name }}</span>
+          </el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
+        <el-menu-item v-else :index="item.frontpath">
+          <el-icon>
+            <component :is="item.icon"></component>
+          </el-icon>
+          <span>{{ item.name }}</span>
         </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
-      </el-menu>
+      </template>
+    </el-menu>
   </div>
 </template>
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter();
+
+const asideMenus = [
+  {
+    name: "后台面板",
+    icon: "help",
+    child: [
+      {
+        name: "主控台",
+        icon: "home-filled",
+        frontpath: "/",
+      },
+    ],
+  },
+  {
+    name: "商城管理",
+    icon: "shopping-bag",
+    child: [
+      {
+        name: "商品管理",
+        icon: "shopping-cart-full",
+        frontpath: "/goods/list",
+      },
+    ],
+  },
+];
+
+const handleSelect = (e) => {
+  router.push(e)
+}
+</script>
+
+<style scoped>
+.f-menu {
+  width: 250px;
+  top: 64px;
+  bottom: 0;
+  left: 0;
+  overflow: auto @apply shadow-md fixed bg-light-50;
+}
+</style>
